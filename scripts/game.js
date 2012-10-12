0ac7b62542e6693dc90c39008202ff4ba087c27a
Troy Miles <rockncoder@gmail.com>
2012-10-12 07:29:52 -0700

    Added explosions and levels.

--- scripts/game.js
+++ scripts/game.js
@@ -27,24 +27,55 @@ var RocknCoder = RocknCoder || {};
         NPC_BULLET: 3
     };
 
+
     RocknCoder.GameLoop = (function (canvas) {
         var spriteImg = RocknCoder.Game.spriteSheet,
+            init = true,
             intervalId,
             context = canvas.getContext("2d"),
-            fastMatrix = [];
+            fastMatrix = [],
+            calcRect = function(sprite){
+                var rect = {};
+                rect.top = sprite.yPos;
+                rect.bottom = sprite.yPos + sprite.height;
+                rect.left = sprite.xPos;
+                rect.right = sprite.xPos + sprite.width;
+                return rect;
+            },
+            shrinkRectangle = function (rect) {
+                var shrink = 10;
+                rect.top += shrink;
+                rect.left += shrink;
+                rect.right -= shrink;
+                rect.bottom -= shrink;
+                return rect;
+            },
+            rectanglesCollide = function (rect1, rect2) {
+                return !(rect1.bottom < rect2.top ||
+                   rect1.top > rect2.bottom ||
+                   rect1.left > rect2.right ||
+                   rect1.right < rect2.left);
+            };
 
         /* if anything flips this bit to false, the level is considered done */
+        RocknCoder.Game.lives = 3;
         RocknCoder.Game.playing = true;
-        RocknCoder.InitNPCs(RocknCoder.Game.level);
-        RocknCoder.InitPlayer(RocknCoder.Game.level);
+        RocknCoder.InitExplosion();
 
         /* this is the actual game loop */
         intervalId = setInterval(function() {
-            var len, ndx, sm, sprite;
+            var ndx, inner, sm, sprite, sprite2, rect1, rect2;
             /* clear the canvas */
             canvas.width = canvas.width;
             fastMatrix = [];
-            fastMatrix.push(null);
+//            fastMatrix.push(null);
+            if(init) {
+                init = false;
+                RocknCoder.SpriteMap = {};
+                RocknCoder.Game.enemies = RocknCoder.Levels[RocknCoder.Game.level].npcs.length;
+                RocknCoder.InitNPCs(RocknCoder.Game.level);
+                RocknCoder.InitPlayer(RocknCoder.Game.level);
+            }
 
             /* draw all of the sprites */
             for(sm in RocknCoder.SpriteMap) {
@@ -60,24 +91,51 @@ var RocknCoder = RocknCoder || {};
 
             /* collision detection */
             for(ndx = fastMatrix.length; ndx; ndx -= 1) {
-                sprite = fastMatrix[ndx];
-                switch(sprite.type) {
+                sprite = fastMatrix[ndx - 1];
+                rect1 = shrinkRectangle(calcRect(sprite));
+
+                 switch(sprite.type) {
                     case RocknCoder.SpriteTypes.PLAYER:
-                       break;
+                        for(inner = fastMatrix.length; inner; inner -= 1) {
+                            sprite2 = fastMatrix[inner - 1];
+
+                            if(sprite2.type === RocknCoder.SpriteTypes.NPC || sprite2.type === RocknCoder.SpriteTypes.NPC_BULLET) {
+                                rect2 = shrinkRectangle(calcRect(sprite2));
+                                if(rectanglesCollide(rect1, rect2)) {
+                                    sprite.killed = true;
+                                    break;
+                                }
+                            }
+                        }
+                        break;
                     case RocknCoder.SpriteTypes.NPC:
+                        for(inner = fastMatrix.length; inner; inner -= 1) {
+                            sprite2 = fastMatrix[inner - 1];
+
+                            if(sprite2.type === RocknCoder.SpriteTypes.PLAYER_BULLET) {
+                                rect2 = calcRect(sprite2);
+                                if(rectanglesCollide(rect1, rect2)) {
+                                    sprite.killed = true;
+                                    sprite2.killed = true;
+                                    break;
+                                }
+                            }
+                        }
                        break;
                 }
             }
 
             /* have we stopped playing? */
             if(!RocknCoder.Game.playing) {
-                /* stop the timer */
-                clearInterval(intervalId);
                 /* if the player still has lives, he won the level */
                 if(RocknCoder.Game.lives) {
-
+                    RocknCoder.Game.playing = true;
+                    RocknCoder.Game.level += 1;
+                    init = true;
                 } else {
-
+                    /* stop the timer */
+                    clearInterval(intervalId);
+                    $.mobile.changePage("#attract");
                 }
             }
         }, 33);
@@ -86,15 +144,41 @@ var RocknCoder = RocknCoder || {};
     RocknCoder.InitNPCs = (function (gameLevel) {
         var i, move, ndx, npcName, x, y,
             lvl = RocknCoder.Levels[gameLevel],
+            explodeMe = function (sprite) {
+                var map;
+                sprite.count += 1;
+                if(sprite.count === 7) {
+                    delete RocknCoder.SpriteMap[sprite.data];
+                    RocknCoder.Game.enemies -= 1;
+                    if(!RocknCoder.Game.enemies) {
+                        RocknCoder.Game.playing = false;
+                    }
+                } else {
+                    map = RocknCoder.Explosion.map[sprite.count];
+                    sprite.xSprite = map[0];
+                    sprite.ySprite = map[1];
+                }
+            },
+            killMe = function (sprite) {
+                if(sprite.killed) {
+                    sprite.count = 0;
+                    sprite.moveFunc = explodeMe;
+                }
+            },
             moves = {
                 none: null,
+                doNothing: function (sprite) {
+                    killMe(sprite);
+                },
                 flyDown: function (sprite) {
                     sprite.yPos += 5;
                     if(sprite.yPos >= RocknCoder.Game.dims.height){ sprite.yPos = -sprite.height;}
+                    killMe(sprite);
                 },
                 spinAround: function (sprite) {
                     sprite.rotate += (360/60);
                     if(sprite.rotate >= 360){ sprite.rotate = 0;}
+                    killMe(sprite);
                 }
             };
 
@@ -114,6 +198,32 @@ var RocknCoder = RocknCoder || {};
             yPos = Math.floor(RocknCoder.Game.dims.height - 64 + 2),
             xTarget = xPos,
             player = null,
+            explodeMe = function (sprite) {
+                var map;
+                sprite.count += 1;
+                if(sprite.count === 7) {
+                    RocknCoder.Game.lives--;
+                    if(RocknCoder.Game.lives) {
+                        sprite.killed = false;
+                        sprite.moveFunc = moves.toNewPosition;
+                        sprite.xSprite = 5;
+                        sprite.ySprite = 401;
+                        sprite.xPos = xTarget = Math.floor((RocknCoder.Game.dims.width  / 2) - 32);
+                    } else {
+                        RocknCoder.Game.playing = false;
+                    }
+                } else {
+                    map = RocknCoder.Explosion.map[sprite.count];
+                    sprite.xSprite = map[0];
+                    sprite.ySprite = map[1];
+                }
+            },
+            killMe = function (sprite) {
+                if(sprite.killed) {
+                    sprite.count = 0;
+                    sprite.moveFunc = explodeMe;
+                }
+            },
             moves = {
                 toNewPosition: function (sprite) {
                     var currXPos = sprite.xPos,
@@ -124,10 +234,11 @@ var RocknCoder = RocknCoder || {};
                         currXPos += diff;
                     }
                     sprite.xPos = currXPos;
+                    killMe(sprite);
                 },
                 flyBullet: function (sprite) {
                     sprite.yPos -= 5;
-                    if(sprite.yPos < 0) {
+                    if(sprite.yPos < 0 || sprite.killed) {
                         /* remove the bullet */
                         delete RocknCoder.SpriteMap[sprite.data];
                         bullet--;
@@ -139,7 +250,7 @@ var RocknCoder = RocknCoder || {};
         player = RocknCoder.SpriteMap.gunShip;
 
         /* the game is playable by touch or mouse */
-        $(window).on('click touchstart ', function(event){
+        $(window).off('click touchstart').on('click touchstart', function(event){
             var bulletName = "playerBullet" + bullet,
                 x = event.pageX,
                 y = event.pageY;
@@ -159,12 +270,29 @@ var RocknCoder = RocknCoder || {};
         });
     });
 
+    RocknCoder.InitExplosion = (function () {
+        RocknCoder.Explosion = {
+            width: 66,
+            height: 66,
+            map: [
+                [  4, 302],
+                [ 72, 302],
+                [136, 302],
+                [202, 302],
+                [268, 302],
+                [334, 302],
+                [400, 302]
+            ]
+        };
+    });
+
+
     RocknCoder.Levels = {
         0: {
             npcs: [
                 [50, 50, "flyDown"],
                 [100, 50, "spinAround"],
-                [150, 50, "none"]
+                [150, 50, "doNothing"]
             ]
         },
         1: {
